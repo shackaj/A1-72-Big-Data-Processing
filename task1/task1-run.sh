48fec86dcbe3c5d91c116efeab40c0831a1c0ac8
a4 <alruhailya@gmail.com>
2026-08-16 22:21:55 +1000

    Update task1-run.sh
    bash shell script

--- task1/task1-run.sh
+++ task1/task1-run.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+chmod +x mapper.py reducer.py
+hadoop fs -rm -r -f /Output/task1
+
+hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \
+    -D mapreduce.job.reduces=3 \
+    -files mapper.py,reducer.py \
+    -mapper "python3 mapper.py" \
+    -reducer "python3 reducer.py" \
+    -input /Input/Trips.txt \
+    -output /Output/task1 

258f4a7fba0751846cb1e10f0f34b166736eb927
Kelly Jones <s4094286@student.rmit.edu.au>
2026-08-16 14:57:10 +1000

    Reformat repo, pushing untested reducer (will test by 17/08)

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # A1-72-Big-Data-Processing
+
+##### Team: Kelly Jones, Aisha Alruhaily, Beyza Ozdamar
+
+
+
+Develop and implement MapReduce programs that demonstrate your understanding of MapReduce principles
+by solving complex data processing problems on the Hadoop execution platform.
+
+# Task 1
+
+HDFS Input/Output Directory Structure
+• /Input/Trips.txt
+• /Output/task1
+
+Output Format
+Contains exactly 6 fields, separated by a tab (\t) character, in the following order:
+
+1. taxi ID
+2. trip type (long, medium, or short)
+3. the total number of trips (integer)
+4. the maximum fare (decimal with two places)
+5. the minimum fare (decimal with two places)
+6. the average fare per trip (decimal with two places)
+
+Files: 
+
+- mapper.py
+- reducer.py
+- task1-run.sh (Shell Script)
---
+++ task1/reducer.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import sys
+
+current_key = None
+current_value = None
+
+# format function to help clean output
+def format_output(key, count, max_fare, min_fare, total):
+    taxi_id, trip_type = key.split('_', 1) # split key into necessary format
+    # get avg fare and handle 0
+    if count:
+        avg_fare = total / count
+    else:
+        avg_fare = 0
+        
+    print(f"{taxi_id}\t{trip_type}\t{count}\t{max_fare}\t{min_fare}\t{avg_fare}")
+
+for line in sys.stdin:
+    
+    line = line.strip()
+    if not line:
+        continue
+    
+    key, stats = line.split("\t")
+    count, max_fare, min_fare, total = stats.split(',')
+    
+    count = int(count)
+    max_fare = float(max_fare)
+    min_fare = float(min_fare)
+    total = float(total)
+    
+    # merge partial into running
+    if key == current_key:
+        current_count += count
+        current_max = max(current_max, max_fare)
+        current_min = min(current_min, min_fare)
+        current_total += total
+    
+    else:
+        # key change = output the final stats for key
+        if current_key is not None:
+            format_output(current_key, current_count, current_max, current_min, current_total)
+        
+        # start running again/or intially
+        current_key = key
+        current_count = count
+        current_max = max_fare
+        current_min = min_fare
+        current_total = total
+
+# output last key
+if current_key is not None:
+    format_output(current_key, current_count, current_max, current_min, current_total)

c4433d0b8b1db3e555ea2291b274786cff405581
BeyzaOzdamar <s4093475@student.rmit.edu.au>
2026-08-28 17:25:43 +1000

    clarifying input

--- task4/task4-run.sh
+++ task4/task4-run.sh
@@ -2,6 +2,8 @@
 
 chmod +x mapper.py reducer.py
 chmod +x task4-run.sh
+hadoop fs -mkdir -p /Input
+hadoop fs -put -f data/Trips.txt /Input/Trips.txt
 hadoop fs -rm -r -f /Output/task4
 
 hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \
